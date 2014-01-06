6d8bd3ad58f5e0765f198dff1565b0dfbcfae821
josecolella <josecolella@yahoo.com>
2014-01-06 14:13:53 +0100

    Eliminated shortest path and added bfs algorithm

--- grados_separacion.py
+++ grados_separacion.py
@@ -5,55 +5,27 @@
 import shelve
 
 
-def find_shortest_path(graph, start, end, path=[]):
+def bfs(graph, start, end):
     """
-    Conseguir el camino más corto. Este algoritmo esta implementado
-    en los tutoriales de python sobre los gráfos.
+    Devuelve el camino que denota una busqueda de breadth first
+    search desde <start> a <end>
 
-    http://www.python.org/doc/essays/graphs/
-    @return tuple Tupla con el primer parametro que es la lista de relaciones
-    de una persona a otra
+    @param start Comienzo de la busqueda
+    @param end Nodo final
+    @return list Camino desde start a end
     """
-    path = path + [start]
-    if start == end:
-        return path
-    if start not in graph:
-        return None
-    shortest = None
-    for node in graph[start]:
-        if node not in path:
-            newpath = find_shortest_path(graph, node, end, path)
-            if newpath:
-                if not shortest or len(newpath) < len(shortest):
-                    shortest = newpath
-    return shortest
-
-
-def getActorsWorkedWith(person):
-    """
-    Devuelve un diccionario que contiene los actores/actrices de los
-    cuales ha trabajado un actor
-
-    @return dict Diccionario con los actores/actrices con los cuales
-    ha trabajado en actor/actriz
-    """
-    try:
-        moviedb = shelve.open('degree.bin')
-        db = shelve.open('moviedb.bin')
-        actorsWorkedWith = []
-        for i in db[person]:
-            filmNameYear = '{} {}'.format(i[0], i[1])
-            if filmNameYear in moviedb:
-                for j in moviedb[filmNameYear]:
-                    if j != person:
-                        actorsWorkedWith.append(j)
-            else:
-                print("NO")
-        db.close()
-        moviedb.close()
-        return actorsWorkedWith
-    except KeyError:
-        print("La persona no esta en la base de datos")
+    bfs = []
+    bfs.append([start])
+    while bfs:
+        path = bfs.pop(0)
+        node = path[-1]
+        if node == end:
+            return path
+        for adjacent in graph.get(node, []):
+            new_path = list(path)
+            new_path.append(adjacent)
+            bfs.append(new_path)
+    return bfs
 
 
 def degreeSeparation(person1, person2):
@@ -66,25 +38,20 @@ def degreeSeparation(person1, person2):
     de relaciones de la primera persona a la segunda. El segundo parametro
     es un entero que representa el número de grados
     """
-    person1ActorsWorkedWith = {person1: getActorsWorkedWith(person1)}
-    person1Copy = person1ActorsWorkedWith.copy()
-    for j in person1ActorsWorkedWith[person1]:
-        if j not in person1Copy:
-            person1Copy[j] = getActorsWorkedWith(j)
-    person1ActorsWorkedWith = person1Copy
-    del person1Copy
-    shortestPath = find_shortest_path(
-        person1ActorsWorkedWith, person1, person2)
+    db = shelve.open('degree.bin')
+    shortestPath = bfs(db, person1, person2) if len(
+        db[person1]) < len(db[person2]) else bfs(db, person2, person1)
     try:
         return shortestPath, len(shortestPath) - 1
     except TypeError:
         print("No hay camino entre {} y {}".format(person1, person2))
+    db.close()
 
 if __name__ == '__main__':
 
     # Las personas a buscar
-    person1 = 'Rachel Appleton'
-    person2 = "Kristen Hager"
+    person1 = 'Kristen Hager'
+    person2 = 'Rachel Appleton'
     shortestPath, length = degreeSeparation(person1, person2)
     print("Camino entre {} y {}".format(person1, person2))
     print("El camino más corto es: {}".format(shortestPath))
